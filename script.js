9676fef2c6e1f6bcb5fe9450f83cc324b097fad0
jose <josesaa0324@gmail.com>
2024-10-27 16:13:24 -0500

    resultado con mensaje alert (final)

--- script.js
+++ script.js
@@ -5,5 +5,15 @@ let hora = fecha.getHours();
 
 let hora1;
 
+if (hora >= 6 && hora < 12) {
+    hora1 = "Buenos días";
+} else if (hora >= 12 && hora < 18) {
+    hora1 = "Buenas tardes";
+} else {
+    hora1 = "Buenas noches";
+}
 
 
+
+
+alert(hora1);
